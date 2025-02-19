fc7e4cf85be69bd864433df6159252a53782b3d7
Isatis <isatis@foxes.gay>
2025-02-19 15:42:41 -0800

    Longer term move from MUI to Mantine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,25 @@
+import '@mantine/core/styles.css';
+
 import Providers from './providers';
 import { useStoresLoaded } from '../context/store';
 import { HashRouter } from 'react-router-dom';
 import Root from '../routes/root';
+import { MantineProvider } from '@mantine/core';
 
 export default function App() {
     const loaded = useStoresLoaded();
 
     return (
-        <HashRouter>
-            <Providers>
-                { loaded ?
-                <Root />
-                :
-                <p>Loading...</p>
-                }
-            </Providers>
-        </HashRouter>
+        <MantineProvider>
+            <HashRouter>
+                <Providers>
+                    { loaded ?
+                    <Root />
+                    :
+                    <p>Loading...</p>
+                    }
+                </Providers>
+            </HashRouter>
+        </MantineProvider>
     )
 }
